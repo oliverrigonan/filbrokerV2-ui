72200821acfb1a5937a47afa1320bf35a0360997
Dick Roldan Alambra <dkalambra143@gmail.com>
2021-03-02 15:54:26 +0800

    User list edit delete function

--- src/app/software/component/system-user-detail/system-user-detail.component.ts
+++ src/app/software/component/system-user-detail/system-user-detail.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { SysDropdownModel } from './../../model/sys-dropdown.model';
+import { MstUserModel } from './../../model/mst-user.model';
+
+import { SysDropdownService } from './../../service/sys-dropdown/sys-dropdown.service';
+import { MstUserService } from './../../service/mst-user/mst-user.service';
+
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-system-user-detail',
@@ -7,9 +20,123 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemUserDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private mstUserService: MstUserService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private sysDropdownService: SysDropdownService,
+    private toastr: ToastrService,
+    private confirmationDeleteDialog: MatDialog,
+  ) { }
 
-  ngOnInit(): void {
+
+  public isSpinnerShow: boolean = true;
+  public isContentShow: boolean = false;
+
+  public mstUserModel: MstUserModel = new MstUserModel();
+  public sysDropdownModel: SysDropdownModel[] = [];
+
+  public isUserSaveButtonDisabled: boolean = false;
+  public isUserLockButtonDisabled: boolean = false;
+  public isUserUnlockButtonDisabled: boolean = false;
+
+  public getDropdownList(): void {
+    this.sysDropdownService.getDropdownList("USER STATUS").subscribe(
+      data => {
+        this.sysDropdownModel = data;
+
+        let id = this.route.snapshot.params['id'];
+        this.getUserDetail(id);
+      }
+    );
   }
 
+  public getUserDetail(id: number): void {
+    this.mstUserService.getUserDetail(id).subscribe(
+      data => {
+
+        setTimeout(() => {
+          if (data != null) {
+            this.mstUserModel.Id = data.Id;
+            this.mstUserModel.Username = data.Username;
+            this.mstUserModel.FullName = data.FullName;
+            this.mstUserModel.Password = data.Password;
+            this.mstUserModel.Status = data.Status;
+          
+            this.isSpinnerShow = false;
+            this.isContentShow = true;
+          }
+        }, 500);
+      }
+    );
+  }
+
+  public disabledButtons(): void {
+    this.isUserSaveButtonDisabled = true;
+    this.isUserLockButtonDisabled = true;
+    this.isUserUnlockButtonDisabled = true;
+  }
+
+  public isLockedButtons(isLocked: boolean): void {
+    this.isUserSaveButtonDisabled = isLocked;
+    this.isUserLockButtonDisabled = isLocked;
+    this.isUserUnlockButtonDisabled = !isLocked;
+  }
+  
+  public buttonSaveUser(): void {
+    this.disabledButtons();
+
+    this.mstUserService.saveUser(this.mstUserModel).subscribe(
+      data => {
+
+        if (data[0] == true) {
+          this.toastr.success('User was successfully saved!', 'Save Successful');
+        } else {
+          this.toastr.error(data[1], 'Save Failed');
+        }
+
+        // this.isLockedButtons(this.mstUserModel.IsLocked);
+      }
+    );
+  }
+  public buttonLockUser(): void {
+    // this.mstUserModel.IsLocked = true;
+    this.disabledButtons();
+
+    this.mstUserService.lockUser(this.mstUserModel).subscribe(
+      data => {
+
+        if (data[0] == true) {
+          this.toastr.success('User was successfully locked!', 'Lock Successful');
+        } else {
+          this.toastr.error(data[1], 'Lock Failed');
+          // this.mstUserModel.IsLocked = false;
+        }
+
+        // this.isLockedButtons(this.mstUserModel.IsLocked);
+      }
+    );
+  }
+
+  public buttonUnlockUser(): void {
+    this.disabledButtons();
+
+    this.mstUserService.unlockUser(this.mstUserModel).subscribe(
+      data => {
+
+        if (data[0] == true) {
+          this.toastr.success('User was successfully unlocked!', 'Unlock Successful');
+          // this.mstUserModel.IsLocked = false;
+        } else {
+          this.toastr.error(data[1], 'Unlock Failed');
+          // this.mstUserModel.IsLocked = true;
+        }
+
+        // this.isLockedButtons(this.mstUserModel.IsLocked);
+      }
+    );
+  }
+  ngOnInit(): void {
+    this.getDropdownList();
+  }
 }
--- src/app/software/component/system-user-list/system-user-list.component.ts
+++ src/app/software/component/system-user-list/system-user-list.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { MstUserModel } from './../../model/mst-user.model';
+import { MstUserService } from './../../service/mst-user/mst-user.service';
+
+import { ToastrService } from 'ngx-toastr';
+
+import { ConfirmationDeleteComponent } from './../confirmation-delete/confirmation-delete.component';
 
 @Component({
   selector: 'app-system-user-list',
@@ -7,9 +20,140 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemUserListComponent implements OnInit {
 
-  constructor() { }
+  public userDisplayedColumns: string[] = [
+    'ButtonEdit',
+    'ButtonDelete',
+    'Username',
+    'FullName',
+    'Status',
+    'Space'
+  ];
 
-  ngOnInit(): void {
+  public userDataSource: MatTableDataSource<MstUserModel>;
+  public userData: MstUserModel[] = []
+
+  @ViewChild('userPaginator') public userPaginator: MatPaginator;
+  @ViewChild('userSort') public userSort: MatSort;
+
+  public isButtonAddUserDisabled: boolean = false;
+
+  constructor(private mstUserService: MstUserService,
+    private router: Router,
+    private toastr: ToastrService,
+    private confirmationDeleteDialog: MatDialog) { }
+
+  public isSpinnerShow: boolean = true;
+  public isContentShow: boolean = false;
+
+  public geUserData(): void {
+    this.userData = [];
+    this.userDataSource = new MatTableDataSource(this.userData);
+    this.userDataSource.paginator = this.userPaginator;
+    this.userDataSource.sort = this.userSort;
+
+    this.mstUserService.getUserList().subscribe(
+      data => {
+        if (data.length > 0) {
+          this.userData = data;
+          this.userDataSource = new MatTableDataSource(this.userData);
+          this.userDataSource.paginator = this.userPaginator;
+          this.userDataSource.sort = this.userSort;
+        }
+
+        this.isSpinnerShow = false;
+        this.isContentShow = true;
+      }
+    );
   }
 
+  public userFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.userDataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.userDataSource.paginator) {
+      this.userDataSource.paginator.firstPage();
+    }
+  }
+
+  public buttonAddUser(): void {
+    this.isButtonAddUserDisabled = true;
+
+    let mstUserModel: MstUserModel = {
+      Id: 0,
+      Username: "",
+      FullName: "",
+      Password: "",
+      Status: "OPEN",
+    };
+
+    this.mstUserService.addUser(mstUserModel).subscribe(
+      data => {
+
+        if (data[0] == true) {
+          if (data[1] > 0) {
+            this.toastr.success('User was successfully added!', 'Add Successful');
+
+            setTimeout(() => {
+              this.router.navigate(['/software/setup-user-detail/' + data[1]]);
+            }, 500);
+          } else {
+            this.toastr.error('Somethings went wrong!', 'Add Failed');
+            this.isButtonAddUserDisabled = false;
+          }
+        } else {
+          this.toastr.error(data[1], 'Add Failed');
+          this.isButtonAddUserDisabled = false;
+        }
+
+      }
+    );
+  }
+
+  public buttonEditUser(currentData: any): void {
+    let id = currentData.Id;
+    this.router.navigate(['/software/system-user-detail/' + id]);
+  }
+  
+
+  public buttonDeleteUser(currentData: any): void {
+
+    let id = currentData.Id;
+
+    const openDialog = this.confirmationDeleteDialog.open(ConfirmationDeleteComponent, {
+      width: '450px',
+      data: {
+        dialogDeleteTitle: "Delete User",
+        dialogDeleteMessage: "Are you sure you want to delete this user " + currentData.Username + "?",
+        dialogDeleteId: id
+      },
+      disableClose: true
+    });
+
+    openDialog.afterClosed().subscribe(result => {
+
+      if (result != null) {
+        this.isSpinnerShow = true;
+        this.isContentShow = false;
+
+        this.mstUserService.deleteUser(result).subscribe(
+          data => {
+
+            if (data[0] == true) {
+              this.toastr.success('User was successfully deleted!', 'Delete Successful');
+              this.geUserData();
+            } else {
+              this.toastr.error(data[1], 'Delete Failed');
+
+              this.isSpinnerShow = false;
+              this.isContentShow = true;
+            }
+
+          }
+        );
+      }
+    });
+  }
+  ngOnInit(): void {
+    this.geUserData();
+  }
 }
